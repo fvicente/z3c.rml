7e4218129aae4de00795346304faa0b4a53fc8f2
fvicente <fvicente@mwneo.com>
2015-10-01 10:54:58 -0300

    pkg_resource might not be available

--- src/z3c/rml/__init__.py
+++ src/z3c/rml/__init__.py
@@ -2,8 +2,11 @@
 import z3c.rml.paraparser
 import z3c.rml.rlfix
 
-import pkg_resources
-__version__ = pkg_resources.require('z3c.rml')[0].version
+try:
+    import pkg_resources
+    __version__ = pkg_resources.require('z3c.rml')[0].version
+except:
+    pass
 
 from reportlab.lib.styles import getSampleStyleSheet
 SampleStyleSheet = getSampleStyleSheet()
